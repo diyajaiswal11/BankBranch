f76ebf73b824e1e6641267820c51c507d3473c64
Atul singh <59124269+atulyaatul1999@users.noreply.github.com>
2020-10-04 01:18:08 +0530

    Update predict.py

--- bankapp/predict.py
+++ bankapp/predict.py
@@ -1,31 +1,14 @@
-from pickle5 import pickle
-from torchvision import transforms
-from PIL import Image
+import keras
+from keras.preprocessing import image
+import numpy as np
 
-def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
+model=keras.models.load_model('./model.h5')
 
-
-img_size = 224
-
-transform = transforms.Compose([
-    transforms.Resize((img_size, img_size)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                        std=[0.229, 0.224, 0.225])
-])
-
-model = pickle.load(open('./new_model.sav', 'rb'))
 
 def predict(img_path):
-    img = Image.open(img_path)
-    img = transform(img)
-    img = img.view(1, 3, img_size, img_size)
-    out = model(img)
-    out=out.detach().numpy()
-    a=softmax(out)
-    prob, pred = np.max(a),np.argmax(a)
-    labels = ['Benign', 'Malignant']
-    return pred, prob * 100
+    img=image.load_img(img_path,target_size=(299,299))
+    img=image.img_to_array(img)/255
+    img=img.reshape(1,299,299,3)
+    out = model.predict_proba(img)
+    prob=np.max(out)
+    return prob * 100
